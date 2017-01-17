e1116c75ab4bec3fa4f5761c31f56bb0cfaf0def
Chris Rudge <chrisrudge2002@gmail.com>
2017-01-17 17:53:46 +0000

    Refactor middleware

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const middleware = require('./middleware.js');
 const app = express();
 const PORT = 3000;
 
-app.get('/about', function (req, res) {
-	res.send('About Us');
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+	res.send('About Us!');
 });
 
 app.use(express.static(`${__dirname}/public`));
 
 app.listen(PORT, function () {
-	console.log('Express server started!');
+	console.log(`Express server started ${PORT}!`);
 });
